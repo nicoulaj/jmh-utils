6caa078d132f5fa0d149611bda55595a98a77410
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2018-04-08 13:56:19 +0200

    Update license headers

--- jmh-assertions/src/main/java/net/nicoulaj/jmh/assertions/package-info.java
+++ jmh-assertions/src/main/java/net/nicoulaj/jmh/assertions/package-info.java
@@ -1,9 +1,7 @@
 /*
- * ====================================================================
- * JMH utils :: assertions
- * ====================================================================
- * Copyright (C) 2014 Julien Nicoulaud <julien.nicoulaud@gmail.com>
- * ====================================================================
+ * JMH utils - http://nicoulaj.github.com/jmh-utils
+ * Copyright © 2014-2018 JMH utils contributors
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
@@ -17,7 +15,6 @@
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
- * ====================================================================
  */
 /**
  * <a href="https://code.google.com/p/fest">FEST</a>/<a href="http://joel-costigliola.github.io/assertj">AssertJ</a> style assertions for <a href="http://openjdk.java.net/projects/code-tools/jmh">JMH</a>.
--- jmh-profilers/jmh-profiler-jhiccup/src/main/java/net/nicoulaj/jmh/profilers/JHiccupProfiler.java
+++ jmh-profilers/jmh-profiler-jhiccup/src/main/java/net/nicoulaj/jmh/profilers/JHiccupProfiler.java
@@ -1,9 +1,7 @@
 /*
- * ====================================================================
- * JMH utils :: profilers :: jHiccup
- * ====================================================================
- * Copyright (C) 2014 Julien Nicoulaud <julien.nicoulaud@gmail.com>
- * ====================================================================
+ * JMH utils - http://nicoulaj.github.com/jmh-utils
+ * Copyright © 2014-2018 JMH utils contributors
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
@@ -17,7 +15,6 @@
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
- * ====================================================================
  */
 package net.nicoulaj.jmh.profilers;
 
